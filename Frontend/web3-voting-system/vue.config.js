f74735407776d804b7ac69ac0bff57963e9a454a
Xiaofan He <xxh395@case.edu>
2024-03-27 16:13:13 -0700

    build the environment and framework for vue

---
+++ Frontend/web3-voting-system/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+const { VantResolver } = require("unplugin-vue-components/resolvers")
+const ComponentsPlugin = require("unplugin-vue-components/webpack").default
+module.exports = defineConfig({
+  transpileDependencies: true, 
+  configureWebpack: {
+    plugins: [
+      ComponentsPlugin({
+        resolvers: [VantResolver()],
+      }),
+    ]
+  }
+})
